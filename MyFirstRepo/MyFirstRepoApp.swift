93e637243e2a64a36196290c70b52a25b5265419
Ethan Chow <nusethan@gmail.com>
2026-08-31 16:12:26 +0800

    Initial Commit

---
+++ MyFirstRepo/MyFirstRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstRepoApp.swift
+//  MyFirstRepo
+//
+//  Created by Ethan Chow  on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
